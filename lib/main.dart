b050046c5c80ba7f0521b9b7c8b5103d2063117c
youksimgyu <dbrtlarb@gmail.com>
2024-07-15 20:56:17 +0900

    theme 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wabiz_client/theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: wabizDefaultTheme,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
