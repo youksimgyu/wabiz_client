d2f18b6baf28309b5b80312be4f020d9ebae6c10
youksimgyu <dbrtlarb@gmail.com>
2024-07-16 20:47:35 +0900

    model 생성

---
+++ lib/models/home/home_model.dart
@@ -0,0 +1,43 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_model.freezed.dart';
+part 'home_model.g.dart';
+
+@freezed
+class HomeModel with _$HomeModel {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory HomeModel({
+    int? totalCount,
+    @Default([]) List<HomeItemModel> projects,
+  }) = _HomeModel;
+
+  factory HomeModel.fromJson(Map<String, dynamic> json) =>
+      _$HomeModelFromJson(json);
+}
+
+@freezed
+class HomeItemModel with _$HomeItemModel {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory HomeItemModel({
+    int? id,
+    int? categoryId,
+    int? projectTypeId,
+    String? userId,
+    String? title,
+    String? owner,
+    int? price,
+    String? thumbnail,
+    String? count,
+    String? deadline,
+    String? description,
+    int? waitlistCount,
+    int? totalFundedCount,
+    int? totalFunded,
+    String? isOpen,
+    String? category,
+    String? type,
+  }) = _HomeItemModel;
+
+  factory HomeItemModel.fromJson(Map<String, dynamic> json) =>
+      _$HomeItemModelFromJson(json);
+}
